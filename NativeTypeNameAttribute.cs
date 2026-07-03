f5a647eb772c9d846680fa373828968bac01b270
Hadrian Tang <hadrianwttang@outlook.com>
2026-07-04 02:37:55 +0800

    Add missing attribute

--- NativeTypeNameAttribute.cs
+++ NativeTypeNameAttribute.cs
@@ -1,5 +1,6 @@
 using System;
 
+[assembly: System.Runtime.CompilerServices.DisableRuntimeMarshalling]
 namespace Box3D
 {
     // -c;generate-helper-types can generate this type, but it's incompatible with -c;single-file (default) right now
